187515100d90e25bbf21e5dd61fba2e554c2a1f7
Vinay-Kamma <kammavinaybabu@gmail.com>
2024-09-14 15:51:05 +0000

    Diabetes Predection model is done

--- DiabetesPrediction/DiabetesPrediction.py
+++ DiabetesPrediction/DiabetesPrediction.py
@@ -1,1 +1,16 @@
-print('hello')
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn import  svm
+from sklearn.metrics import accuracy_score
+df=pd.read_csv('DiabetesPrediction/Dataset/diabetes.csv')
+sc=StandardScaler()
+input_variables=df.drop(columns=['Outcome'])
+output_variables=df[['Outcome']]
+input_variables=sc.fit_transform(input_variables)
+X_train,X_test,Y_train,Y_test=train_test_split(input_variables,output_variables,test_size=0.2,stratify=output_variables,random_state=1)
+model=svm.SVC(kernel='linear')
+model.fit(X_train,Y_train)
+print("train Accuracy:",accuracy_score(model.predict(X_train),Y_train))
+print("test Accuracy:",accuracy_score(model.predict(X_test),Y_test))
